9be2ac896603934782af140635bbdd3f42ea51cb
Elizabeth <65304506+EliSauKu@users.noreply.github.com>
2021-01-19 14:59:36 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,6 +1,16 @@
 
-window.alert("Hello and welcome to my ideal Judicial system page! ✴️\nClick on the different buttons and I will present my proposals in detail.")
-
+window.onload=function(){
+Swal.fire({
+    icon: "info",
+    title: '✴️ Hello and welcome to my ideal Judicial system page! ✴️\n\nClick on the different colour buttons and I will present my proposals in detail.',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  })
+}
 // Redirects you to the ideal system, explained in detail :D
 
 $ = function(id) {
@@ -8,10 +18,9 @@ $ = function(id) {
 }
 
 var show = function(id) {
-	$(id).style.display ='block';
+	$(id).style.display ='block'; bvvvvvvvvvvvvvb
 }
 var hide = function(id) {
 	$(id).style.display ='none';
 }
-
 
