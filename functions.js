93d90354a06ac4134a876416de5fbff3dcf08d1b
Elizabeth <65304506+EliSauKu@users.noreply.github.com>
2021-01-19 15:06:11 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,6 +1,17 @@
 
-window.alert("Hello World")
 
+window.onload=function(){
+Swal.fire({
+    icon: "info",
+    title: '✴️ Hello and welcome to my ideal Judicial system page! ✴️\n\nClick on the different colour buttons and I will present my proposals in detail.',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  })
+}
 // Redirects you to the ideal system, explained in detail :D
 
 $ = function(id) {
@@ -8,7 +19,7 @@ $ = function(id) {
 }
 
 var show = function(id) {
-	$(id).style.display ='block';
+	$(id).style.display ='block'; bvvvvvvvvvvvvvb
 }
 var hide = function(id) {
 	$(id).style.display ='none';
