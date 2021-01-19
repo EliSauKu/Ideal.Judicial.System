2058f493240f18d8dc1faeda305da06a54c91e3e
Elizabeth <65304506+EliSauKu@users.noreply.github.com>
2021-01-19 12:54:19 +0100

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,15 @@
+
+window.alert("Hello World")
+
+// Redirects you to the ideal system, explained in detail :D
+
+$ = function(id) {
+  return document.getElementById(id);
+}
+
+var show = function(id) {
+	$(id).style.display ='block';
+}
+var hide = function(id) {
+	$(id).style.display ='none';
+}
