cd49b8d822852a1c9178fba7a42fe36c129ce1e1
Elizabeth <65304506+EliSauKu@users.noreply.github.com>
2021-01-19 13:17:35 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 
-window.alert("Hello and welcome to my ideal Judicial system page! ✴️\nClick on the different colour buttons and I will present my proposals in detail.")
+window.alert("✴️ Hello and welcome to my ideal Judicial system page! ✴️\nClick on the different colour buttons and I will present my proposals in detail.")
 
 // Redirects you to the ideal system, explained in detail :D
 
